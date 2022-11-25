b579eeeba6cc061f0525ad24fb10b01b224a0f79
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2022-11-25 23:57:38 +0530

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,12 +10,12 @@ function Navbar() {
 
   <Link className="navbar-link"  Exact from="/"   to="/"> <img className="logo" width="100" src={require('../images/calorie-finder.png')} alt="" /></Link>
 
-  <Link className="navbar-link text-white p-2" to='/'>Home</Link>
-  <Link className="navbar-link text-white p-2" to='/product'>Product</Link>
+  <Link className="navbar-link text-white p-2" to='meal-tracker/'>Home</Link>
+  <Link className="navbar-link text-white p-2" to='meal-tracker/product'>Product</Link>
 
     <Routes>
-      <Route exact path="/" element={<MealsPlan/>} />
-      <Route path='/product' element={<GroceryProduct/>} />
+      <Route exact path="meal-tracker/" element={<MealsPlan/>} />
+      <Route path='meal-tracker/product' element={<GroceryProduct/>} />
     </Routes>
 
   </div>
